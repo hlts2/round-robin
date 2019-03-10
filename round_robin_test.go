fcf0d9ac01a81e618bc30cbc651d80e4958ca10c
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2019-03-10 21:46:02 +0900

    Tweak

--- round_robin_test.go
+++ round_robin_test.go
@@ -1,56 +1,53 @@
 package roundrobin
 
-// func TestRoundRobin(t *testing.T) {
-// 	tests := []struct {
-// 		servers   []string
-// 		count     int
-// 		errExists bool
-// 		expected  []string
-// 	}{
-// 		{
-// 			servers: []string{
-// 				"server-1",
-// 				"server-2",
-// 				"server-3",
-// 			},
-// 			count:     4,
-// 			errExists: false,
-// 			expected: []string{
-// 				"server-1",
-// 				"server-2",
-// 				"server-3",
-// 				"server-1",
-// 			},
-// 		},
-// 		{
-// 			servers:   []string{},
-// 			count:     0,
-// 			errExists: true,
-// 			expected:  []string{},
-// 		},
-// 	}
-//
-// 	for i, test := range tests {
-// 		rr, err := New(test.servers)
-//
-// 		errExists := !(err == nil)
-//
-// 		if test.errExists != errExists {
-// 			t.Errorf("tests[%d] - RoundRobin errExists is wrong. expected: %v, got: %v", i, test.errExists, errExists)
-// 		}
-//
-// 		gots := make([]string, 0, test.count)
-//
-// 		for j := 0; j < test.count; j++ {
-// 			gots = append(gots, rr.Next())
-// 		}
-//
-// 		for j, expected := range test.expected {
-// 			got := gots[j]
-//
-// 			if got != expected {
-// 				t.Errorf("tests[%d] - RoundRobin is wrong. expected: %v, got: %v", i, expected, got)
-// 			}
-// 		}
-// 	}
-// }
+import (
+	"net/url"
+	"reflect"
+	"testing"
+)
+
+func TestRoundRobin(t *testing.T) {
+	tests := []struct {
+		urls     []*url.URL
+		iserr    bool
+		expected []string
+		want     []*url.URL
+	}{
+		{
+			urls: []*url.URL{
+				{Host: "192.168.33.10"},
+				{Host: "192.168.33.11"},
+				{Host: "192.168.33.12"},
+			},
+			iserr: false,
+			want: []*url.URL{
+				{Host: "192.168.33.10"},
+				{Host: "192.168.33.11"},
+				{Host: "192.168.33.12"},
+				{Host: "192.168.33.10"},
+			},
+		},
+		{
+			urls:  []*url.URL{},
+			iserr: true,
+			want:  []*url.URL{},
+		},
+	}
+
+	for i, test := range tests {
+		rr, err := New(test.urls)
+
+		if got, want := !(err == nil), test.iserr; got != want {
+			t.Errorf("tests[%d] - RoundRobin iserr is wrong. want: %v, but got: %v", i, test.want, got)
+		}
+
+		gots := make([]*url.URL, 0, len(test.want))
+		for j := 0; j < len(test.want); j++ {
+			gots = append(gots, rr.Next())
+		}
+
+		if got, want := gots, test.want; !reflect.DeepEqual(got, want) {
+			t.Errorf("tests[%d] - RoundRobin is wrong. want: %v, got: %v", i, want, got)
+		}
+	}
+}
